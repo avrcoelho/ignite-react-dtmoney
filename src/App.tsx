d1219006f5799ebd29213c21722d5c5cf72c503c
andre-coelho-axur <andre.coelho@axur.com>
2021-03-16 12:55:58 -0300

    style: global

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,10 @@
+import { GlobalStyles } from "./assets/styles/global";
+
 export function App() {
-  return <h1>Hello world</h1>;
+  return (
+    <>
+      <GlobalStyles />
+      <h1>Hello world</h1>
+    </>
+  );
 }
