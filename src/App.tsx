468f4c26535ff26cfbd000638ba84d183c22bd72
avrcoelho <andrevrcoelho@hotmail.com>
2021-03-25 17:17:55 -0300

    feat: mirageJS server

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
+import { createServer } from "miragejs";
+
 import Dashboard from "./components/Dashboard";
 import Header from "./components/Header";
-
 import { GlobalStyles } from "./assets/styles/global";
+
+createServer({
+  routes() {
+    this.namespace = "api";
+
+    this.get("/transactions", () => {
+      return [
+        {
+          id: 1,
+          title: "transaction 1",
+          amount: 400,
+          type: "deposit",
+          category: "Food",
+          date: "2021-02-02",
+        },
+      ];
+    });
+  },
+});
 
 export function App() {
   return (
